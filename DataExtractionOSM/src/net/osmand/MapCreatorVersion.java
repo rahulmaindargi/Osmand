f7500e885dc07418faa8e1159d2bc005fa5ed70c
Victor Shcherb <victor.shcherb@gmail.com>
2012-08-26 18:55:43 +0200

    Preparation for 0.8.2

--- DataExtractionOSM/src/net/osmand/MapCreatorVersion.java
+++ DataExtractionOSM/src/net/osmand/MapCreatorVersion.java
@@ -5,8 +5,8 @@ import java.net.URLEncoder;
 
 public class MapCreatorVersion {
 	
-	public static String APP_VERSION = "0.8.3"; //$NON-NLS-1$
-	public static final String APP_DESCRIPTION = "alpha"; //$NON-NLS-1$
+	public static String APP_VERSION = "0.8.2"; //$NON-NLS-1$
+	public static final String APP_DESCRIPTION = "beta"; //$NON-NLS-1$
 
 	public static final String APP_MAP_CREATOR_NAME = "OsmandMapCreator"; //$NON-NLS-1$
 	public static final String APP_MAP_CREATOR_VERSION = APP_MAP_CREATOR_NAME + " " + APP_VERSION; //$NON-NLS-1$
